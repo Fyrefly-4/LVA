8c2ed78b1381342e3a2a2c06687d52fae4c21bc1
Risey <2233035417@qq.com>
2026-05-28 15:00:58 +0800

    feat(frontend): 登录流程编写成功，系统鉴权功能初步可用

--- frontend/LightVueAdmin/src/views/login/index.vue
+++ frontend/LightVueAdmin/src/views/login/index.vue
@@ -1,33 +1,72 @@
 <script setup>
 
+    import { reactive } from 'vue'
     import { loginAPI } from '@/api/auth'
+    import { useRouter } from 'vue-router'
     import { ElMessage } from 'element-plus'
 
-    const handleLogin = async () => {
-    try {
-        const data = await loginAPI({
-        username: 'admin',
-        password: 'password123'
-        })
-        console.log('联调成功！拿到JWT令牌了:', data.token)
-        localStorage.setItem('token', data.token)
-        ElMessage.success('提示：前后端纯 JS 联调成功，数据握手完毕！')
-    } catch (error) {
-        console.error(error)
+    const router = useRouter()
+    const form = reactive({
+        username: '',
+        password: ''
+    })
+
+    const onLogin = async () => {
+        //基础校验：防止用户未输入就点击登录
+        if(!form.username || !form.password) {
+            ElMessage.warning("请填写完整信息")
+            return
+        }
+
+        try {
+            //调用接口
+            const data = await loginAPI(form)
+            //成功后，将登录令牌存到浏览器本地缓存
+            localStorage.setItem('token', data.token)
+            ElMessage.success("登录成功")
+            //路由跳转至首页
+            router.push('/')
+
+        } catch (error) {
+            //
+        }
+
     }
-    }
+
 
 </script>
 
 <template>
 
-    <div style="padding: 100px; text-align: center;">
-        <h2>MyAdmin 后台管理系统（纯JS版）骨架已就绪</h2>
-        <el-button type="primary" @click="handleLogin">一键联调后端种子数据</el-button>
+    <div class="login-container">
+        <el-card>
+            <h3>系统登录</h3>
+
+            <el-form :model="form">
+                <el-form-item>
+                    <el-input v-model="form.username" placeholder="请输入用户名" />
+                </el-form-item>
+                    
+                <el-form-item>
+                    <el-input v-model="form.password" type="password" placeholder="请输入密码" />
+                </el-form-item>
+                
+                <el-button type="primary" style="width: 100%" @click="onLogin" >登录</el-button>
+            </el-form>
+        </el-card>
     </div>
 
 </template>
 
 <style scoped>
-
+    .login-container {
+        height: 100vh; 
+        display: flex; /* flex盒子布局 */
+        justify-content: center;
+        align-items: center;
+        background-color: #f0f2f5;
+    }
+    .login-card {
+        width: 450px;
+    }
 </style>
